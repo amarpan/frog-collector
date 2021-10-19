710e9e005400c41a246da8c06bb27b97affe8de0
Amar Panjwani <amar.panjwani@gmail.com>
2021-10-18 23:19:51 -0700

    finish index with fake db data

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('frogs/', views.frogs_index, name='index'),
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,27 @@
+# Add the Cat class & list and view function below the imports
+class Frog:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, name, species, description, age):
+    self.name = name
+    self.species = species
+    self.description = description
+    self.age = age
+
+frogs = [
+  Frog('Lolo', 'poison dart', 'innocent killer', 3),
+  Frog('Sachi', 'kermit', 'big ribbit', 0),
+  Frog('Raven', 'black tiller', 'spotted toad', 4)
+]
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Home</h1>')
-# Create your views here.
+
+def about(request):
+    return render(request, 'about.html')
+
+def frogs_index(request):
+    return render(request, 'frogs/index.html', {
+        'frogs': frogs
+    })
